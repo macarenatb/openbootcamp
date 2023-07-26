9e94b37d476c2e9bc6825522e7017573f4c58382
unknown <macarenatb13@gmail.com>
2023-07-26 10:45:41 -0300

    primer entrega

--- src/Main.java
+++ src/Main.java
@@ -1,80 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-/*        System.out.println(sumatoria(4,23,2 ));
-        System.out.println(dameText());*/
 
-        Coche auto1 = new Coche();
-        Coche auto2 = new Coche();
+        int resultado = suma(1, 2, 4);
+        System.out.println(resultado);
 
-        auto1.modificarCoche("Renault", 4 );
-
-
-
-        System.out.println(auto1.name);
-        System.out.println(auto1.puertas);
-
-        System.out.println(auto2.name);
-        System.out.println(auto2.puertas);
-
-    Cat cat1 = new Cat();
-    cat1.giveName("malita");
-    cat1.setYears(3);
-    cat1.setFemale(true);
-
-    cat1.showProps();
-
-
-
-    Cat cat2 = new Cat();
-    cat2.giveName("scotish");
-    cat2.setYears(4);
-    cat2.setFemale(true);
-
-    cat2.showProps();
+        Coche miCoche = new Coche();
+        miCoche.aumentarPuertas(1);
+        System.out.println(miCoche.puertas);
     }
-}
+    public static int suma (int a, int b, int c) {
+            return a+b+c;
+    }
 
+}
 class Coche {
-    String name = "nuevo";
-    int puertas = 0;
+        int puertas = 0;
 
-
-    public void giveName(String newName){
-        this.name = newName;
+        public void aumentarPuertas(int numPuertas) {
+            this.puertas = this.puertas + numPuertas;
+        }
     }
 
-    public void incrementarPuertas () {
-        this.puertas++;
-    }
-    public void aumentarPuertas (int a){
-        this.puertas = this.puertas + a;
-    }
-    public void modificarCoche (String newName, int puertas){
-        this.name = newName;
-        this.puertas = puertas;
-    }
-}
-class Cat {
-    String name = "michi";
-    int years = 0;
-    boolean isFemale = true;
-
-   public void giveName (String newName) {
-       this.name = newName;
-   }
-
-   public void setYears (int years) {
-
-       this.years = years;
-   }
-
-    public void setFemale(boolean female) {
-        this.isFemale = female;
-    }
-
-    public void showProps(){
-        System.out.println(this.name);
-        System.out.println(this.isFemale);
-        System.out.println(this.years);
-    }
-}
